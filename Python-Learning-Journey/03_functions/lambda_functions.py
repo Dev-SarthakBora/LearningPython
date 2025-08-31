fd13403c8dce724964ac1708a296b1f096d54047
Dev-SarthakBora <borasarthak275@gmail.com>
2025-08-31 09:43:58 +0530

    Update lambda_functions.py

--- Python-Learning-Journey/03_functions/lambda_functions.py
+++ Python-Learning-Journey/03_functions/lambda_functions.py
@@ -1,1 +1,51 @@
 # lambda_functions.py
+# just uncomment the part you want to run 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------#
+# List of students with (name, marks)
+
+# students = [
+#     ("Sarthak", 85),
+#     ("Anita", 92),
+#     ("Rahul", 76),
+#     ("Meena", 89)
+# ]
+
+# # Sort by marks using lambda
+# students_sorted = sorted(students, key=lambda x: x[1], reverse=True)
+
+# print("Students sorted by marks (highest first):")
+# for name, marks in students_sorted:
+#     print(f"{name} -> {marks}")
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+# # Original prices
+# prices = [250, 400, 150, 100]
+
+# # Apply 10% discount using lambda
+# discounted_prices = list(map(lambda p: p * 0.9, prices))
+
+# print("Original Prices:", prices)
+# print("Discounted Prices:", discounted_prices)
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+# List of (customer_name, age, salary)
+# customers = [
+#     ("Ravi", 25, 30000),
+#     ("Neha", 30, 50000),
+#     ("Arjun", 22, 15000),
+#     ("Pooja", 28, 45000)
+# ]
+
+# # Eligible if salary >= 30000
+# eligible = list(filter(lambda c: c[2] >= 30000, customers))
+
+# print("Customers eligible for loan:")
+# for name, age, salary in eligible:
+#     print(f"{name} (Age: {age}, Salary: {salary})")
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+
