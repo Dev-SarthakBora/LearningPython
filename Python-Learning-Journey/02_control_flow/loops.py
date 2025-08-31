a08d5e40cca925ecf2330d0d1e39b474a3d42028
Dev-SarthakBora <borasarthak275@gmail.com>
2025-08-31 09:35:42 +0530

    Update loops.py

--- Python-Learning-Journey/02_control_flow/loops.py
+++ Python-Learning-Journey/02_control_flow/loops.py
@@ -1,1 +1,20 @@
 # loops.py
+n = int(input("Enter a number: "))
+total = 0
+
+for i in range(1, n + 1):
+    total += i
+
+print("Sum of first", n, "numbers is:", total)
+
+
+num = int(input("Enter a number: "))
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num //= 10
+
+print("Reversed number is:", reverse)
+
