01239f2f63e3ee2af675c86098717b4f3d498a68
Dev-SarthakBora <borasarthak275@gmail.com>
2025-08-31 09:35:03 +0530

    Update nested_loops.py

--- Python-Learning-Journey/02_control_flow/nested_loops.py
+++ Python-Learning-Journey/02_control_flow/nested_loops.py
@@ -1,1 +1,6 @@
 # nested_loops.py
+for i in range(1, 6):          # outer loop for rows
+    for j in range(1, 6):      # inner loop for columns
+        print(i * j, end="\t") # print product in table format
+    print()  # new line after each row
+
