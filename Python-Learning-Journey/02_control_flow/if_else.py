d455c9bc94f3d50e1e99040c2ca2e6ac8da5a535
Dev-SarthakBora <borasarthak275@gmail.com>
2025-08-31 09:36:07 +0530

    Update if_else.py

--- Python-Learning-Journey/02_control_flow/if_else.py
+++ Python-Learning-Journey/02_control_flow/if_else.py
@@ -1,1 +1,12 @@
 # if_else.py
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 50:
+    print("Grade: C")
+else:
+    print("Grade: Fail")
+
