995726e82de3e230e80428caf3f80d7f4e338c67
Dev-SarthakBora <borasarthak275@gmail.com>
2025-08-31 09:37:48 +0530

    Update pattern_programs.py

--- Python-Learning-Journey/02_control_flow/pattern_programs.py
+++ Python-Learning-Journey/02_control_flow/pattern_programs.py
@@ -1,1 +1,14 @@
 # pattern_programs.py
+rows = 5
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
+
+#output
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+
