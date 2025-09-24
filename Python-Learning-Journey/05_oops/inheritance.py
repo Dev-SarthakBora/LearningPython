1346db884eab2f148ce65165c102751c053c8961
Sarthak Bora <borasarthak275@gmail.com>
2025-09-24 12:08:13 +0530

    Update inheritance.py

--- Python-Learning-Journey/05_oops/inheritance.py
+++ Python-Learning-Journey/05_oops/inheritance.py
@@ -1,1 +1,14 @@
 # inheritance.py
+# Parent class
+class Animal:
+    def speak(self):
+        print("Some generic sound")
+
+# Child class
+class Dog(Animal):
+    def speak(self):
+        print("Woof! Woof!")
+
+dog = Dog()
+dog.speak()  # Woof! Woof!
+
