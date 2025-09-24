82d8f513880fe77ca4d29fca42ad8b43f3d43b7e
Sarthak Bora <borasarthak275@gmail.com>
2025-09-24 12:07:32 +0530

    Update classes_objects.py

--- Python-Learning-Journey/05_oops/classes_objects.py
+++ Python-Learning-Journey/05_oops/classes_objects.py
@@ -1,1 +1,17 @@
 # classes_objects.py
+# Class definition
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def display_info(self):
+        print(f"Car: {self.brand} {self.model}")
+
+# Creating objects
+car1 = Car("Toyota", "Corolla")
+car2 = Car("Honda", "Civic")
+
+car1.display_info()
+car2.display_info()
+
