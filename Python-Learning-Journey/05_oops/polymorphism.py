bc8b8ad7733f6c0d36d32ebdca251e474d8d590f
Sarthak Bora <borasarthak275@gmail.com>
2025-09-24 12:09:20 +0530

    Update polymorphism.py

--- Python-Learning-Journey/05_oops/polymorphism.py
+++ Python-Learning-Journey/05_oops/polymorphism.py
@@ -1,1 +1,19 @@
 # polymorphism.py
+class Cat:
+    def speak(self):
+        print("Meow")
+
+class Dog:
+    def speak(self):
+        print("Woof")
+
+# Polymorphism: same method name, different behavior
+def animal_sound(animal):
+    animal.speak()
+
+cat = Cat()
+dog = Dog()
+
+animal_sound(cat)  # Meow
+animal_sound(dog)  # Woof
+
