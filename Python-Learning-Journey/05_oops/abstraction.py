f9a389c553f0e08b7f3e336693870db6664d42fb
Sarthak Bora <borasarthak275@gmail.com>
2025-09-24 12:08:59 +0530

    Update abstraction.py

--- Python-Learning-Journey/05_oops/abstraction.py
+++ Python-Learning-Journey/05_oops/abstraction.py
@@ -1,1 +1,20 @@
 # abstraction.py
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+rect = Rectangle(5, 3)
+print(rect.area())  # 15
+#  like the abstract method implemented in java
